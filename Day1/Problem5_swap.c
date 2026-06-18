bb84fcccd69fcfa60cf56479cef12e96fa2d743a
vanshikajain19 <jainvanshu19@gmail.com>
2026-06-18 17:30:51 +0530

    Create Problem5_swap.c

---
+++ Day1/Problem5_swap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Before swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
