738d9b5a3a17d09bdf45e70bfed0981684eab528
vanshikajain19 <jainvanshu19@gmail.com>
2026-07-09 18:15:27 +0530

    Print_n_numbers.cpp

---
+++ Day3/Print_n_numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     int n;
+
+    cout << "Enter N: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+ return 0;
+}
