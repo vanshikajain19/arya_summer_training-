34dac7592ae007b7e7451e8c86befd1b24c7c29d
vanshikajain19 <jainvanshu19@gmail.com>
2026-07-10 18:17:03 +0530

    Sum.cpp

---
+++ Day4/Sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+      int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d", sum(a, b));
+
+    return 0;
+}
