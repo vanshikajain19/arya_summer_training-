72fc6afb30e6aae915ecdae4213b8ddeeb4fe953
vanshikajain19 <jainvanshu19@gmail.com>
2026-07-13 21:42:42 +0530

    factorial.cpp

---
+++ Day4/factorial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+long long factorial(int n)
+{
+    long long fact = 1;
+
+    for(int i = 1; i <= n; i++)
+        fact *= i;
+
+    return fact;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factorial = %lld", factorial(n));
+
+    return 0;
+}
