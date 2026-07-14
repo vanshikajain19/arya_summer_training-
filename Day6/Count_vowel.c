9511cc59ea4bda015887fe2cef9d2ae96b989ef2
vanshikajain19 <jainvanshu19@gmail.com>
2026-07-14 23:15:36 +0530

    Count_vowel.c

---
+++ Day6/Count_vowel.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[100];
+    int i,count=0;
+
+    printf("Enter String: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+           str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U')
+            count++;
+    }
+
+    printf("Vowels = %d",count);
+
+    return 0;
+}
